c015645e86754f008f8e72414ae2b37647a0265f
Hunter Hodnett <hunterhodnett@hunters-mbp.lan>
2023-03-15 21:58:37 -0400

    save OpenAI API key to workspace settings

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,30 @@
 import * as vscode from "vscode";
 import generateUnitTest from "./create-unit-test";
 
+const API_KEY_SETTING = "generateUnitTest.apiKey";
+
 export async function activate(context: vscode.ExtensionContext) {
+  let apiKey = vscode.workspace.getConfiguration().get(API_KEY_SETTING);
+  if (!apiKey) {
+    apiKey = await vscode.window.showInputBox({
+      prompt: "Please enter your OpenAI API key",
+      ignoreFocusOut: true,
+    });
+    if (apiKey) {
+      await vscode.workspace
+        .getConfiguration()
+        .update(API_KEY_SETTING, apiKey, vscode.ConfigurationTarget.Global);
+    } else {
+      vscode.window.showErrorMessage("OpenAI API key not provided");
+      return;
+    }
+  }
+
   const disposable = vscode.commands.registerCommand(
     "generateUnitTest.generate",
     async (uri: vscode.Uri) => {
-      const apiKey = await vscode.window.showInputBox({
-        prompt: "Please enter your OpenAI API key",
-        ignoreFocusOut: true,
-      });
-
-      if (apiKey) {
-        const componentPath = uri.fsPath;
-        await generateUnitTest(componentPath, apiKey);
-      } else {
-        vscode.window.showErrorMessage("OpenAI API key not provided");
-      }
+      const componentPath = uri.fsPath;
+      await generateUnitTest(componentPath, apiKey as string);
     }
   );
 
